72e5fadfae5a8b482248967fa02fae324e4ad014
bruhmoment654 <temaaaya@gmail.com>
2025-03-11 13:44:30 +0300

    feat: custom tween example

--- lib/animation.dart
+++ lib/animation.dart
@@ -13,6 +13,7 @@ class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
     with SingleTickerProviderStateMixin {
   late final Animation<double> animation;
 
+  late final Animation<Cat> catAnimation;
   @override
   void initState() {
     super.initState();
@@ -20,10 +21,16 @@ class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
     final controller = AnimationController(vsync: this)
       ..repeat(
         period: const Duration(seconds: 1),
+        reverse: true
       );
 
-    animation = CurvedAnimation(parent: controller, curve:  const Cubic(.18,-1.06,0,.43));
+    animation = CurvedAnimation(
+        parent: controller, curve: const Cubic(.18, -1.06, 0, .43));
 
+    catAnimation = CatTween(
+      begin: const Cat(Colors.orange),
+      end: const Cat(Colors.pink),
+    ).animate(controller);
   }
 
   @override
@@ -31,14 +38,38 @@ class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
     return Center(
       child: AnimatedBuilder(
         animation: animation,
-        builder: (_, __) => Transform.rotate(
-          angle: math.pi * animation.value * 2,
-          child: const Icon(
-            Icons.autorenew,
-            size: 100,
-          ),
-        ),
+        builder: (_, __) {
+
+          return Container(
+            width: 200,
+            height: 200,
+            color: catAnimation.value.color,
+            child: Transform.rotate(
+              angle: math.pi * animation.value * 2,
+              child: const Icon(
+                Icons.autorenew,
+                size: 100,
+              ),
+            ),
+          );
+        },
       ),
+    );
+  }
+}
+
+class Cat {
+  final Color? color;
+  const Cat(this.color);
+}
+
+class CatTween extends Tween<Cat> {
+  CatTween({super.begin, super.end});
+
+  @override
+  Cat lerp(double t) {
+    return Cat(
+      Color.lerp(begin?.color, end?.color, t),
     );
   }
 }
