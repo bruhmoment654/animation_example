1cd55dba5e405cd26694b6fe3daf91c24f485c7f
bruhmoment654 <temaaaya@gmail.com>
2025-03-11 13:05:53 +0300

    feat: staggered animation

--- lib/animation.dart
+++ lib/animation.dart
@@ -11,33 +11,60 @@ class ArrowAnimatationWidget extends StatefulWidget {
 
 class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
     with SingleTickerProviderStateMixin {
-  late final Animation<double> animation;
+  late final AnimationController controller;
+  late final Animation<double> rotateAnimation;
+  late final Animation<double> moveAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    final controller = AnimationController(vsync: this)
+    controller = AnimationController(vsync: this)
       ..repeat(
-        period: const Duration(seconds: 1),
+        period: const Duration(seconds: 2),
+        reverse: true
       );
 
-    animation = CurvedAnimation(parent: controller, curve:  const Cubic(.18,-1.06,0,.43));
+    rotateAnimation = CurvedAnimation(
+        parent: controller, curve: Curves.easeInOutQuart);
 
+    moveAnimation = Tween<double>(
+      begin: 100,
+      end: 200,
+    ).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: const Interval(0.4, 0.6, curve: Curves.easeInOutQuart),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (_, __) => Transform.rotate(
-          angle: math.pi * animation.value * 2,
-          child: const Icon(
-            Icons.autorenew,
-            size: 100,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          AnimatedBuilder(
+            animation: controller,
+            builder: (_, __) => Positioned(
+              left: moveAnimation.value,
+              child: Transform.rotate(
+                angle: math.pi * rotateAnimation.value * 2,
+                child: const Icon(
+                  Icons.autorenew,
+                  size: 100,
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
