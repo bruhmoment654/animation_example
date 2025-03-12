07cfe899d0ec1707cff4d2d0e85f38bdf57f03be
bruhmoment654 <temaaaya@gmail.com>
2025-03-12 16:51:24 +0300

    feat: lottie example

--- lib/animation.dart
+++ lib/animation.dart
@@ -1,44 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-import 'dart:math' as math;
-
-class ArrowAnimatationWidget extends StatefulWidget {
-  const ArrowAnimatationWidget({super.key});
-
-  @override
-  State<ArrowAnimatationWidget> createState() => _ArrowAnimatationWidgetState();
-}
-
-class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
-    with SingleTickerProviderStateMixin {
-  late final Animation<double> animation;
-
-  @override
-  void initState() {
-    super.initState();
-
-    final controller = AnimationController(vsync: this)
-      ..repeat(
-        period: const Duration(seconds: 1),
-      );
-
-    animation = CurvedAnimation(parent: controller, curve:  const Cubic(.18,-1.06,0,.43));
-
-  }
+class AnimationExample extends StatelessWidget {
+  const AnimationExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (_, __) => Transform.rotate(
-          angle: math.pi * animation.value * 2,
-          child: const Icon(
-            Icons.autorenew,
-            size: 100,
-          ),
-        ),
-      ),
+      child: Lottie.asset('assets/animation.json'),
     );
   }
 }
