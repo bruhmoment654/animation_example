4ad29f601d27a7202d837b22117af3b27da3f6b6
bruhmoment654 <temaaaya@gmail.com>
2025-03-13 12:02:39 +0300

    feat: shader example

--- lib/animation.dart
+++ lib/animation.dart
@@ -1,44 +1,83 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-import 'dart:math' as math;
-
-class ArrowAnimatationWidget extends StatefulWidget {
-  const ArrowAnimatationWidget({super.key});
+class ShaderAnimation extends StatefulWidget {
+  const ShaderAnimation({super.key});
 
   @override
-  State<ArrowAnimatationWidget> createState() => _ArrowAnimatationWidgetState();
+  State<ShaderAnimation> createState() => _ShaderAnimationState();
 }
 
-class _ArrowAnimatationWidgetState extends State<ArrowAnimatationWidget>
+class _ShaderAnimationState extends State<ShaderAnimation>
     with SingleTickerProviderStateMixin {
-  late final Animation<double> animation;
+  late final AnimationController controller;
 
   @override
   void initState() {
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 5))
+          ..repeat(reverse: true);
+
     super.initState();
+  }
 
-    final controller = AnimationController(vsync: this)
-      ..repeat(
-        period: const Duration(seconds: 1),
-      );
+  Future<FragmentShader> _loadShader(String path) async {
+    final fragment = await FragmentProgram.fromAsset(path).catchError((e) {
+      debugPrint(e);
+    });
 
-    animation = CurvedAnimation(parent: controller, curve:  const Cubic(.18,-1.06,0,.43));
-
+    return fragment.fragmentShader();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedBuilder(
-        animation: animation,
-        builder: (_, __) => Transform.rotate(
-          angle: math.pi * animation.value * 2,
-          child: const Icon(
-            Icons.autorenew,
-            size: 100,
-          ),
-        ),
-      ),
-    );
+    return FutureBuilder(
+        future: _loadShader('assets/test.frag'),
+        builder: (_, snapshot) {
+          final shader = snapshot.data;
+          if (shader == null) return const SizedBox();
+    
+          return CustomPaint(
+            size: MediaQuery.sizeOf(context),
+            painter: ShaderPainter(
+              animation: controller,
+              shader: shader,
+            ),
+          );
+        });
   }
+}
+
+class ShaderPainter extends CustomPainter {
+  final Animation<double> animation;
+  final FragmentShader shader;
+
+  const ShaderPainter({required this.animation, required this.shader})
+      : super(repaint: animation);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    shader
+      ..setFloat(0, animation.value * 10)
+      ..setFloat(1, size.width)
+      ..setFloat(2, size.height);
+
+    final paint = Paint()..shader = shader;
+
+    final textPainter = TextPainter(
+      text: TextSpan(
+        text: 'SOME TEXT',
+        style: TextStyle(fontSize: 100, foreground: paint),
+      ),
+      textDirection: TextDirection.ltr,
+    )..layout();
+
+    textPainter.paint(canvas, Offset.zero);
+
+    // canvas.drawPaint(paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
